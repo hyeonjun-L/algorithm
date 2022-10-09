351e3808fbdac17193fcdc3aefb38630804aa265
HyeonJun <beeminor60@gmail.com>
2022-10-09 22:01:26 +0900

    Revert "solve"
    This reverts commit 35edde65335ab660a87cbf8b9fbfdccea6af7028.

--- progrmas/Level1/문자열 내 p와 y 개수.js
+++
@@ -1,3 +0,0 @@
-function solution(s){
-    return ((s.split('p').length - 1 + s.split('P').length - 1 === s.split('y').length - 1 + s.split('Y').length - 1) ? true : false);
- }
--- progrmas/Level1/제일 작은수 제거하기.js
+++
@@ -1,3 +0,0 @@
-function solution(arr) {
-    return arr.length > 1 ? arr.filter(v => v > Math.min(...arr)) : [-1]
-}
