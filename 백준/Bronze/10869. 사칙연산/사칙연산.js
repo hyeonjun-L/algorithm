3e6d245b37e1f8cec6c46369839f3089b94c3276
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-10-16 22:20:18 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(' ').map(Number)
+answer = []
+
+answer[0] = input[0] + input[1]
+answer[1] = input[0] - input[1]
+answer[2] = input[0] * input[1]
+answer[3] = Math.floor(input[0] / input[1])
+answer[4] = input[0] % input[1]
+
+console.log(answer.join('\n'))
