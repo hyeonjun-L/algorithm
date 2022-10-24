4172575fb8aa48f520f37808101e8d37606ca06d
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-10-24 23:25:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,7 +1,9 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number)
 input.shift()
 let answer = [1, 2, 4, 7, 13, 24, 44, 81, 149, 274]
-
+let result = []
 for(let i = 0; i < input.length; i++){
-    console.log(answer[input[i] - 1])
+    result[result.length] = answer[input[i] - 1]
 }
+
+console.log(result.join('\n'))
