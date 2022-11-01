d4be8e46bb73eba970fb041c4d5fa7449b873b14
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-11-01 22:16:22 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,4 +1,3 @@
 function solution(phone_number) {
-    const BN4 = phone_number.match(/\d{4}$/)
-    return '*'.repeat(phone_number.length - 4) + BN4
+    return phone_number.replace(/\d(?=\d{4})/g, "*");
 }
