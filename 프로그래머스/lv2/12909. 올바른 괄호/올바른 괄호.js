7f3bfdaea4a1132d1a62dc63a371893984839237
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-11-09 19:29:34 +0900

    [level 2] Title: 올바른 괄호, Time: 5.54 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  let opened = 0;
+  for (const bracket of s) {
+    if (bracket === "(") opened += 1;
+    if (bracket === ")") opened -= 1;
+    if (opened < 0) return false;
+  }
+  return opened === 0;
+}
+//
+//
