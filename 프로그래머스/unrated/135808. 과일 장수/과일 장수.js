195fb6b7f624b6d6a1ba7fde9946ec5a0f62c77a
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-11-12 19:57:13 +0900

    [unrated] Title: 과일 장수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,13 +1,8 @@
 function solution(k, m, score) {
-    score.sort((a,b) => b - a)
-    let result = []
-    let acc = 0
-    for(i=0; i<score.length; i+=m) result.push(score.slice(i, i+m))
-    
-    for(i=0; i<Math.floor(score.length / m); i++)
-    {
-        acc += m * Math.min(...result[i])
+    let answer = 0;
+    const sortedScore = score.sort((a, b) => a - b).slice(score.length % m);
+    for (let i = 0; i < sortedScore.length; i += m) {
+        answer += sortedScore[i] * m;
     }
-
-    return acc
+    return answer;
 }
