62ee19c3f6b6ec0c1e7067c45cdca547eadd7ab1
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-11-28 18:04:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,25 +1,10 @@
-function solution(n, words){
-    let dropout = 0;
-    let number = 0;
+function solution(n, words) {
+    let answer = 0;
+    words.reduce((prev, now, idx) => {
+        answer = answer || ((words.slice(0, idx).indexOf(now) !== -1 || prev !== now[0]) ? idx : answer);
+        return now[now.length-1];
+    }, "")
 
-    if(words.length !== new Set(words).size){
-        number = Infinity;
-        for(let i = 0; i <= words.length - 1; i++){
-            let wordsnum = words.indexOf(words[i],i+1)
-            if(wordsnum !== -1 && number > wordsnum) number = wordsnum
-        }
-        number++
-    }
-
-    let arr = words.join(" ").match(/[a-z](?=\s)|(?<=\s)[a-z]/g)
-    for(let i = 0; i <= arr.length - 1; i+=2){
-        if(arr[i] !== arr[i + 1]){
-            if(!number) number = i/2+2
-            else if(number > i/2+2) number = i/2+2
-        }
-    }
-
-    if(!number) return [0,0]
-    else return [number % n === 0 ? n : number % n,Math.ceil(number/n)]
+    return answer ? [answer%n+1, Math.floor(answer/n)+1] : [0,0];
 }
 
