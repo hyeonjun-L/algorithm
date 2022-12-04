776d72fd0cbee182253f42102df40b0c7c00891e
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-12-04 19:05:38 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,11 +1,10 @@
 function solution(k, score) {
-    let rank = [];
-    let answer = [];
-    
-    for(let i of score){
-        rank[rank.length] = i;
-        if(rank.length > k) rank[rank.indexOf(Math.min(...rank))] = 2001;
-        answer[answer.length] = Math.min(...rank);
-    }
-    return answer;
-}
+    const stack = []
+    return score.reduce((a, c) => {
+        stack.push(c)
+        stack.sort((a, b) => a - b)
+        if (stack.length > k) stack.shift()
+        a.push(stack[0])
+        return a
+    }, [])
+} 
