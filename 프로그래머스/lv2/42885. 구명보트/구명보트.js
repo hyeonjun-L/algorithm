8e274e06433f04dfb4317fb707b11c69417e45c9
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-12-05 22:45:01 +0900

    [level 2] Title: 구명보트, Time: 11.40 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,31 +1,7 @@
-function solution(peoples, limit) {
-    let bigPeoples = [];
-    let count = 0;
-    peoples.sort((a,b) => a - b);
-    peoples.forEach((people,index) => {
-        if(people > Math.floor(limit / 2)) bigPeoples = peoples.splice(index,peoples.length - 1);
-    });
-    peoples.sort((a,b) => b - a);
-
-    while(bigPeoples.length){
-        if(bigPeoples[bigPeoples.length -1] + peoples[peoples.length -1] <= limit){
-            bigPeoples.pop();
-            peoples.pop();
-            count++;
-        }
-        else{
-            bigPeoples.pop();
-            count++;
-        } 
-    }
-
-    while(peoples.length >= 2){
-        if(peoples[peoples.length -1] + peoples[peoples.length -2] <= limit){
-            peoples.pop();
-            peoples.pop();
-            count++;
-        }
-        else break;
-    }
-    return count + peoples.length;
+function solution(people, limit) {
+    people.sort(function(a, b){return a-b});
+    for(var i=0, j=people.length-1; i < j; j--) {
+        if( people[i] + people[j] <= limit ) i++;
+    }    
+    return people.length-i;
 }
