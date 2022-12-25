67a8303d8cbbcd07fe4812db10b2ef4733db1933
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2022-12-25 16:19:37 +0900

    [level 1] Title: 최소직사각형, Time: 3.71 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,12 +1,4 @@
 function solution(sizes) {
-    let width = 0;
-    let length = 0;
-    const sortsizes = sizes.map(element => {
-        return element[0] < element[1] ? element.reverse() : element; 
-    });
-    sortsizes.forEach(element => {
-        if(element[0] > width) width = element[0];
-        if(element[1] > length) length = element[1];
-    });
-    return width * length;
+    const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
+    return hor * ver;
 }
