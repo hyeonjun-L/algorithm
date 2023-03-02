075740fb8d0a86199e555f8ca79798b5bfa54c9f
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2023-03-03 02:12:17 +0900

    [Silver V] Title: 소수 찾기, Time: 120 ms, Memory: 9620 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.js
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.js
@@ -1,24 +1,29 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split(/\s+/).slice(1);
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require('fs').readFileSync(filePath).toString().trim().split("\n");
 
-solution(input);
-
-function solution(arr) {
-  const max = Math.max(...arr);
-  const isPrime = new Array(max + 1).fill(true);
-  isPrime[0] = false;
-  isPrime[1] = false;
-  for (let i = 2; i <= Math.sqrt(max); i++) {
-    if (isPrime[i]) {
-      for (let j = i * i; j <= max; j += i) {
-        isPrime[j] = false;
-      }
+const N = +input[0];
+let cnt = 0;
+function isPrime(n) {
+    if( n <= 1 )
+        return 0;      
+	
+    if( n % 2 == 0) 
+        return n==2 ? 1 : 0;
+         
+    for(i = 3; i <= Math.sqrt(n); i += 2) { 
+		
+        if( n % i == 0)
+            return 0;
     }
-  }
-
-  let count = 0;
-  for (let num of arr) {
-    if (isPrime[num]) count++;
-  }
-  console.log(count);
+    
+	return 1; 
 }
+
+for (let i = 0; i < N; i++) {
+    let num = input[1].split(" ")[i];
+    if (isPrime(num) == 1) {
+        cnt += 1;
+    }
+}
+
+console.log(cnt);
