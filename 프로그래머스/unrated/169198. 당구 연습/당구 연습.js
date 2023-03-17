189356a7148e13db7e82f65fb5b87ab38e53d00c
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2023-03-17 17:53:59 +0900

    [unrated] Title: 당구 연습, Time: 0.66 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/unrated/169198. 당구 연습/당구 연습.js
+++ 프로그래머스/unrated/169198. 당구 연습/당구 연습.js
@@ -3,23 +3,19 @@ function solution(m, n, startX, startY, balls) {
   for (let ball of balls) {
     const distance = [];
     if (ball[1] !== startY || ball[0] <= startX) {
-      console.log(ball, "1번 포함");
       distance.push(
         ((m - ball[0]) * 2 + ball[0] - startX) ** 2 + (ball[1] - startY) ** 2
       );
     }
     if (ball[1] !== startY || ball[0] >= startX) {
-      console.log(ball, "2번 포함");
       distance.push((-ball[0] - startX) ** 2 + (ball[1] - startY) ** 2);
     }
     if (ball[0] !== startX || ball[1] <= startY) {
-      console.log(ball, "3번 포함");
       distance.push(
         ((n - ball[1]) * 2 + ball[1] - startY) ** 2 + (ball[0] - startX) ** 2
       );
     }
     if (ball[0] !== startX || ball[1] >= startY) {
-      console.log(ball, "4번 포함");
       distance.push((-ball[1] - startY) ** 2 + (ball[0] - startX) ** 2);
     }
     answer.push(Math.min(...distance));
