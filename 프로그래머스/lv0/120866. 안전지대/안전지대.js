206ba063368d31c57a64ead879692d8ac7d5a8b4
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2023-03-31 21:44:32 +0900

    [level 0] Title: 안전지대, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,26 +1,13 @@
 function solution(board) {
-  const dx = [-1, -1, -1, 0, 0, 1, 1, 1];
-  const dy = [-1, 0, 1, -1, 1, -1, 0, 1];
-  const n = board.length;
-  const dangerZone = JSON.parse(JSON.stringify(board));
 
-  for (let x = 0; x < n; x++) {
-    for (let y = 0; y < n; y++) {
-      if (board[x][y] === 0) continue;
+    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
+    let safezone = 0;
 
-      for (let i = 0; i < dx.length; i++) {
-        const nx = x + dx[i];
-        const ny = y + dy[i];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+    board.forEach((row, y, self) => row.forEach((it, x) => {
+        if (it === 1) return false;
+        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
+               ? false : safezone++;
+    }));
 
-        dangerZone[nx][ny] = 1;
-      }
-    }
-  }
-
-  const flatDangerZone = dangerZone.flat();
-  const numDangerCells = flatDangerZone.filter((cell) => cell).length;
-  const numSafeCells = n * n - numDangerCells;
-  
-  return numSafeCells;
+    return safezone;
 }
