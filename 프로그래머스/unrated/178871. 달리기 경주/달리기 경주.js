afa421889d7f856a2ccb6fa3834e95886a0bbf95
Pat <113358733+hyeonjun-L@users.noreply.github.com>
2023-04-10 00:26:53 +0900

    [unrated] Title: 달리기 경주, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,18 +1,17 @@
 function solution(players, callings) {
-  playersObj = {};
-  playersKeyObj = {};
-  players.forEach((player, index) => {
-    playersObj[player] = index;
-    playersKeyObj[index] = player;
-  });
-
-  for (let player of callings) {
-    playersObj[player] -= 1;
-    playersObj[playersKeyObj[playersObj[player]]] += 1;
-
-    const stack = playersKeyObj[playersObj[player]];
-    playersKeyObj[playersObj[player]] = player;
-    playersKeyObj[playersObj[player] + 1] = stack;
+  let maps = {};
+  for (let i = 0; i < players.length; i++) {
+    maps[players[i]] = i;
   }
-  return Object.values(playersKeyObj);
+  callings.forEach((v) => {
+    let num = maps[v];
+    let temp = players[num - 1];
+    players[num - 1] = v;
+    players[num] = temp;
+    maps[v]--;
+    maps[players[num]]++;
+  });
+  return Object.entries(maps)
+    .sort((a, b) => a[1] - b[1])
+    .map((v) => v[0]);
 }
