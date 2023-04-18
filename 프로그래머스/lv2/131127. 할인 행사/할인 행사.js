b1d701e307a72144717729b58b8e06aae4fd1178
Pat <113358733+hyeonjun-L@users.noreply.github.com>
2023-04-18 23:18:04 +0900

    [level 2] Title: 할인 행사, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,26 @@
+function solution(want, number, discount) {
+  const wantObj = want.reduce((acc, curr, index) => {
+    acc[curr] = number[index];
+    return acc;
+  }, {});
+  let answer = 0;
+
+  for (var i = 0; i < discount.length; i++) {
+    if (want.includes(discount[i])) {
+      let copyObj = { ...wantObj };
+      for (let stuff of discount.slice(i, i + 10)) {
+        if (copyObj[stuff]) {
+          copyObj[stuff] -= 1;
+        }
+      }
+      if (
+        Object.values(copyObj).reduce((acc, value) => {
+          return (acc += value);
+        }, 0) === 0
+      ) {
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
