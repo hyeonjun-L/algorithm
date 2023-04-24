a427e9303a93d3f9ae3b04bf54b6a75f5b3fee7c
Pat <113358733+hyeonjun-L@users.noreply.github.com>
2023-04-25 02:48:27 +0900

    [level 2] Title: [3차] 압축, Time: 6.80 ms, Memory: 34.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,25 @@
+function solution(msg) {
+  const dictionary = {};
+  const answer = [];
+  for (let i = 1; i < 27; i++) {
+    dictionary[String.fromCharCode(64 + i)] = i;
+  }
+
+  for (let i = 0; i < msg.length; i++) {
+    let count = 1;
+    let wc = msg[i];
+
+    while (true) {
+      wc += msg[i + count] !== undefined ? msg[i + count] : "X";
+      if (!dictionary[wc]) {
+        dictionary[wc] = Object.values(dictionary).length + 1;
+        answer.push(dictionary[wc.slice(0, -1)]);
+        i += count - 1;
+        break;
+      } else {
+        count++;
+      }
+    }
+  }
+  return answer;
+}
