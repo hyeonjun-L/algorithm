d00f7ea80d7c6a15048eac35d436462688c98a9a
Pat <113358733+hyeonjun-L@users.noreply.github.com>
2023-04-29 02:04:07 +0900

    [level 2] Title: 피로도, Time: 0.62 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,30 +1,4 @@
 function solution(k, dungeons) {
-  let answer = 0;
-
-  function verification(dungeons) {
-    let fatigue = k;
-    let count = 0;
-    for (let dungeon of dungeons) {
-      let [min, waste] = dungeon;
-      if (fatigue < min) break;
-      else {
-        fatigue -= waste;
-        count++;
-      }
-    }
-    answer = Math.max(count, answer);
-  }
-
-  function bfs(i, dungeons) {
-    if (i === dungeons.length) {
-      return verification([...dungeons]);
-    }
-    for (let j = i; j < dungeons.length; j++) {
-      [dungeons[i], dungeons[j]] = [dungeons[j], dungeons[i]];
-      bfs(i + 1, dungeons);
-      [dungeons[i], dungeons[j]] = [dungeons[j], dungeons[i]];
-    }
-  }
-  bfs(0, dungeons);
-  return answer;
+    var answer = -1;
+    return answer;
 }
