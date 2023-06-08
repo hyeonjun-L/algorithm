e8f3d75ce68027362734d49f305780b01899ac09
duckoo <113358733+hyeonjun-L@users.noreply.github.com>
2023-06-09 04:41:26 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,13 @@
 function solution(numbers) {
-    var answer = '';
-    return answer;
+  if (Number(numbers.join("")) === 0) {
+    return "0";
+  }
+
+  return numbers
+    .sort((a, b) => {
+      const aString = `${a}`;
+      const bString = `${b}`;
+      return Number(bString + aString) - Number(aString + bString);
+    })
+    .join("");
 }
