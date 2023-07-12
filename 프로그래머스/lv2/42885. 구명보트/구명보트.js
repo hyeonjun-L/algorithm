fc5d4ad429de461194638e58b799a53b8f27df95
duckoo <113358733+hyeonjun-L@users.noreply.github.com>
2023-07-13 02:59:27 +0900

    [level 2] Title: 구명보트, Time: 34.26 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,7 +1,11 @@
 function solution(people, limit) {
-    people.sort(function(a, b){return a-b});
-    for(var i=0, j=people.length-1; i < j; j--) {
-        if( people[i] + people[j] <= limit ) i++;
-    }    
-    return people.length-i;
+  people.sort((a, b) => a - b);
+
+  let j = people.length - 1;
+  for (var i = 0; i < j; j--) {
+    if (people[i] + people[j] <= limit) {
+      i++;
+    }
+  }
+  return people.length + i - i * 2;
 }
