e2733aa257256e2168d7eea1fbfb14ad63edc2dd
duckoo <113358733+hyeonjun-L@users.noreply.github.com>
2023-07-22 20:56:39 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#qna) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.25 ms
+메모리: 33.5 MB, 시간: 0.29 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,35 +1,29 @@
-function solution(str1, str2) {
-  const arr1 = collect(str1.toUpperCase());
-  const arr2 = collect(str2.toUpperCase());
-  let intersection = 0;
-  let union = 0;
-  let index = arr1.length - 1;
-    
-  if (arr1.length + arr2.length === 0) return 65536;
-  while (arr1.length !== 0) {
-    let include = arr2.indexOf(arr1[index]);
-    if (include !== -1) {
-      union++;
-      intersection++;
-      arr2.splice(include, 1);
-      arr1.pop();
-    } else {
-      union++;
-      arr1.pop();
+function strSplit(str) {
+  const strArray = [];
+  for (let i = 0; i < str.length - 1; i++) {
+    const element = (str[i] + str[i + 1]).toUpperCase();
+    if (/^[A-Z]*$/.test(element)) {
+      strArray.push(element);
     }
-    index--;
   }
-    return Math.floor((intersection / (union + arr2.length)) * 65536);
+  return strArray;
 }
 
-function collect(str) {
-  const arr = [];
-  const regExp = /^[A-Z]*$/;
-  for (let i = 0; i < str.length - 1; i++) {
-    let word = str[i] + str[i + 1];
-    if (regExp.test(word)) {
-      arr.push(word);
+function solution(str1, str2) {
+  str1 = strSplit(str1);
+  str2 = strSplit(str2);
+
+  const totalStrLength = str1.length + str2.length;
+  if (totalStrLength === 0) return 65536;
+
+  const intersection = [];
+  str1.forEach((element) => {
+    const elementIndex = str2.indexOf(element);
+    if (elementIndex !== -1) {
+      str2.splice(elementIndex, 1);
+      intersection.push(element);
     }
-  }
-  return arr;
+  });
+
+  return Math.floor((intersection.length / (totalStrLength - intersection.length)) * 65536);
 }
