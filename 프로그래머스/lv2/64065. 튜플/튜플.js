35c23d3a6925430569bd0ea0f2f94332e7f66357
duckoo <113358733+hyeonjun-L@users.noreply.github.com>
2023-07-25 05:38:01 +0900

    [level 2] Title: 튜플, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,14 +1,33 @@
+/*
 function solution(s) {
-  return [
-    ...new Set(
-      s
-        .replace(/{{|}}/g, '')
+    var answer = [];
+    let sets = s
+        .slice(2,-2)
         .split('},{')
-        .map((str) => str.split(',').sort((a, b) => b - a))
-        .sort((a, b) => a.length - b.length)
-        .join()
-        .split(',')
-        .map((str) => Number(str)),
-    ),
-  ];
+        .map(set => set.split(',').map(x=>parseInt(x)))
+        .sort((a,b)=> a.length-b.length)
+    
+    for(const set of sets) answer.push(...set.filter(x => !answer.includes(x)))
+    return answer
+}   
+
+function solution(s) {
+    s = s.replace(/\{|}}/g, '')
+    .split('},')
+    .map(value => value.split(',').map(Number))
+    .sort((a,b) => a.length - b.length)
+    
+    s.filter((value,index) => s.indexOf(value) === index)
 }
+*/
+
+function solution(s) {
+    s = s.replace(/\{|}}/g, '')
+    .split('},')
+    .sort((a,b) => a.length - b.length)
+    .join()
+    .split(',')
+    
+    return Array.from(new Set(s)).map(Number)
+}
+
