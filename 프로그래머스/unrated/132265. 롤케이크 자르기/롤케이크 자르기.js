4753ae6195e9d0aa5849476fa82182031e4dfbcf
duckoo <113358733+hyeonjun-L@users.noreply.github.com>
2023-07-25 04:30:50 +0900

    [unrated] Title: 롤케이크 자르기, Time: 138.76 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,31 @@
+function setTopping(topping, map) {
+  if (map.has(topping)) {
+    map.set(topping, map.get(topping) + 1);
+  } else {
+    map.set(topping, 1);
+  }
+}
+
+function solution(topping) {
+  const cheolsu = new Map();
+  const brother = new Map();
+
+  topping.forEach((tp) => {
+    setTopping(tp, brother);
+  });
+
+  return topping.reduce((acc, tp) => {
+    setTopping(tp, cheolsu);
+
+    brother.set(tp, brother.get(tp) - 1);
+    if (brother.get(tp) === 0) {
+      brother.delete(tp);
+    }
+
+    if (brother.size === cheolsu.size) {
+      return (acc += 1);
+    } else {
+      return acc;
+    }
+  }, 0);
+}
