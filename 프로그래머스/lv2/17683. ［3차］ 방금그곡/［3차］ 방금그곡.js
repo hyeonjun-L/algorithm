0794f29674e9ae7cc1755cceacbd640acea37222
duckoo <113358733+hyeonjun-L@users.noreply.github.com>
2023-08-02 21:24:01 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.84 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -0,0 +1,49 @@
+function parseMelody(melody) {
+  const melodyMap = {
+    "C#": "H",
+    "D#": "I",
+    "F#": "J",
+    "G#": "K",
+    "A#": "L",
+    "E#": "F",
+    C: "C",
+    D: "D",
+    E: "E",
+    F: "F",
+    G: "G",
+    A: "A",
+    B: "B",
+  };
+
+  return melody.replace(/C#|D#|F#|G#|A#|E#|C|D|E|F|G|A|B/g, (match) => melodyMap[match]);
+}
+
+function solution(m, musicinfos) {
+  const answer = {
+    title: "(None)",
+    totalTime: 0,
+  };
+  m = parseMelody(m);
+
+  musicinfos.forEach((musicinfo) => {
+    const [musicStart, musicEndTime, title, sheetMusic] = musicinfo.split(",");
+
+    const playTime =
+      (new Date(`1970-01-01 ${musicEndTime}`) -
+        new Date(`1970-01-01 ${musicStart}`)) /
+      60000;
+
+    // 확장된 음악을 재생 시간만큼 자름
+    let fullMelody = parseMelody(sheetMusic);
+
+    const repeatedFullMelody = fullMelody.repeat(Math.ceil(playTime / fullMelody.length));
+    fullMelody = repeatedFullMelody.slice(0, playTime);
+
+    if (fullMelody.includes(m) && playTime > answer.totalTime) {
+      answer.title = title;
+      answer.totalTime = playTime;
+    }
+  });
+
+  return answer.title;
+}
