1baed48a41dba44bcd5f5d97f71c34984b495a64
hyeonjun-L <beeminor60@gmail.com>
2024-06-30 21:35:03 +0900

    buildPalindrome

---
+++ codesignal/buildPalindrome.js
@@ -0,0 +1,48 @@
+// Given a string, find the shortest possible string which can be achieved by adding characters to the end of initial string to make it a palindrome.
+
+// Example
+
+// For st = "abcdc", the output should be
+// solution(st) = "abcdcba".
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [memory limit] 1 GB
+
+// [input] string st
+
+// A string consisting of lowercase English letters.
+
+// Guaranteed constraints:
+// 3 ≤ st.length ≤ 10.
+
+// [output] string
+
+function solution(st) {
+  const reverseSt = st.split("").reverse().join("");
+  let buffer = "";
+
+  if (checkString(st)) {
+    return st;
+  }
+
+  for (let i = 0; i < st.length; i++) {
+    buffer += st[i];
+    const newString = buffer + reverseSt;
+    if (checkString(newString)) {
+      return newString;
+    }
+  }
+}
+
+function checkString(st) {
+  for (let i = 0; i < Math.floor(st.length / 2); i++) {
+    if (st[i] !== st[Math.abs(i - st.length + 1)]) {
+      return false;
+    }
+  }
+
+  return true;
+}
